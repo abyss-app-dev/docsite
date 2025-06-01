d5878e224788aa173f4972fb510d80ae6b11c5ca
Eric <abyss@abyss-app.com>
2025-06-01 12:33:12 -0700

    release log

--- src/config.ts
+++ src/config.ts
@@ -19,11 +19,3 @@ export const SIDEBAR = [
   { text: "Getting Started", header: true },
   { text: "Introduction", link: "/getting-started/introduction" },
 ];
-
-export const RELEASE_LOG_SIDEBAR = [
-  { text: "Release Notes", header: true },
-  { text: "Latest Version", link: "/release-log" },
-  { text: "Version History", header: true },
-  { text: "v1.0.0", link: "/release-log#v1-0-0" },
-  { text: "v0.9.0 Beta", link: "/release-log#v0-9-0-beta" },
-];
