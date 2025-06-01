1fd6492bc2d8bd55d05bd96a4683a965bf36accd
Eric <abyss@abyss-app.com>
2025-06-01 11:56:44 -0700

    update 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://docs.abyss.app', // Update this to your actual domain
   integrations: [
     // Enable Vue for Vue components
     vue(),
